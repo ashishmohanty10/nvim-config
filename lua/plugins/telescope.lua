2a2b049aed256fa4a3bfb09aba21926c41fd0069
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-11-12 00:25:03 +0530

    first commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- Disable some default LazyVim keymaps if you want
+      -- { "<leader>ff", false },
+      
+      -- Your custom keymaps
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "[S]earch [H]elp" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "[S]earch [K]eymaps" },
+      { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "[S]earch [F]iles" },
+      { "<leader>ss", "<cmd>Telescope builtin<cr>", desc = "[S]earch [S]elect Telescope" },
+      { "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "[S]earch current [W]ord" },
+      { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "[S]earch by [G]rep" },
+      { "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "[S]earch [D]iagnostics" },
+      { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "[S]earch [R]esume" },
+      { "<leader>s.", "<cmd>Telescope oldfiles<cr>", desc = "[S]earch Recent Files" },
+      { "<leader><leader>", "<cmd>Telescope buffers<cr>", desc = "[ ] Find existing buffers" },
+    },
+  },
+}
