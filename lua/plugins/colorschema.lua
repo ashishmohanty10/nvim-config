2b2fe09d7d81f924f99ebe9c0e9a26bc37cc9d92
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-11-12 03:55:52 +0530

    fix: colorschema lua and comment out indent

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,18 +1,36 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "night",
-    },
-  },
+  "marko-cerovac/material.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Set your theme variant
+    vim.g.material_style = "deep ocean" -- or: darker, lighter, oceanic, pale night, darker
 
-  -- tell LazyVim to use this variant
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight-night",
-    },
-  },
+    require("material").setup({
+      contrast = {
+        terminal = false,
+        sidebars = false,
+        floating_windows = false,
+        cursor_line = false,
+        non_current_windows = false,
+        filetypes = {}, -- filetypes listed here will be `contrast` enabled
+      },
+
+      plugins = {
+        "gitsigns",
+        "nvim-tree",
+        "telescope",
+        "which-key",
+        "dap",
+      },
+
+      high_visibility = {
+        lighter = false,
+        darker = false,
+      },
+    })
+
+    -- Apply the colorscheme
+    vim.cmd("colorscheme material")
+  end,
 }
