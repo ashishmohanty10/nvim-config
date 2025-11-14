a601564ce6913f590e5629cf23558f7cf0ff44b2
ashishmohanty10 <ashishmohaty10@gmail.com>
2025-11-14 07:43:31 +0530

    feat(neotree): update neo-tree and stylua configuration

--- lua/plugins/nvim-neo-tree.lua
+++ lua/plugins/nvim-neo-tree.lua
@@ -8,6 +8,9 @@ return {
           hide_dotfiles = false,
           hide_gitignored = false,
         },
+
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
       },
     },
     init = function()
